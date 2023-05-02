73f1c9c44debba29c95a15ffec0b5291b2dbaa5f
Vashishth Patel <vashishthchaudhary48@gmail.com>
2023-05-02 15:30:33 +0530

    error fixed

--- src/components/logout/logout.js
+++ src/components/logout/logout.js
@@ -3,7 +3,7 @@ import { useCookies } from 'react-cookie';
 import { Redirect } from "react-router-dom";
 
 const logoutuser = (props) => {
-  const [ removeCookie] = useCookies(['user']);
+  const [cookies, removeCookie] = useCookies(['user']);
   // if (cookies.jwttokenloginuser) {
   //   removeCookie('jwttokenloginuser', { path: "/" });
   //   console.log("This is removed!");
